959545652f6e7528f2024a9cc44fb1337f62db15
cboyce428 <cboyce428@gmail.com>
2019-02-16 18:10:35 +0000

    Update Core.cs

--- Xero.Api/Infrastructure/Applications/Public/Core.cs
+++ Xero.Api/Infrastructure/Applications/Public/Core.cs
@@ -1,4 +1,4 @@
-﻿using Xero.Api.Core;
+using Xero.Api.Core;
 using Xero.Api.Infrastructure.Authenticators;
 using Xero.Api.Infrastructure.Interfaces;
 
@@ -6,8 +6,8 @@ namespace Xero.Api.Infrastructure.Applications.Public
 {
     public class Core : XeroCoreApi
     {
-        public Core(PublicAuthenticatorBase authenticator, IUser user, bool includeRateLimiter = false)
-            : base(authenticator, user, includeRateLimiter ? new RateLimiter.RateLimiter() : null)
+        public Core(PublicAuthenticatorBase authenticator, IXeroApiSettings applicationSettings, IUser user, bool includeRateLimiter = false)
+            : base(authenticator, applicationSettings, user, includeRateLimiter ? new RateLimiter.RateLimiter() : null)
         {
         }
     }
